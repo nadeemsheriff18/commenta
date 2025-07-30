20253d19e94fee2ebec7151fc7499ed3fec587eb
Nadeem Sheriff A <137400851+nadeemsheriff18@users.noreply.github.com>
2025-07-30 22:45:00 +0530

    landing page fix1

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -35,7 +35,7 @@ export default function BlogPage() {
   const [isLoading, setIsLoading] = useState(true);
   const [isPostLoading, setIsPostLoading] = useState(false);
 //process.env.NEXT_PUBLIC_API_URL ||
-  const API_BASE_URL ='http://127.0.0.1:8000';
+  const API_BASE_URL =process.env.NEXT_PUBLIC_API_URL ||'http://127.0.0.1:8000';
 
   const formatDate = (dateString: string) => {
     return new Date(dateString).toLocaleDateString("en-US", {
--- lib/api.ts
+++ lib/api.ts
@@ -2,7 +2,7 @@
 
 import Cookies from 'js-cookie';
 //process.env.NEXT_PUBLIC_API_URL ||
-const API_BASE_URL = 'http://127.0.0.1:8000';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL ||'http://127.0.0.1:8000';
 
 // --- INTERFACES ---
 export interface ApiResponse<T = any> {
